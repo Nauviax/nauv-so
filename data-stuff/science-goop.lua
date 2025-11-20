a06c7133bf7ac8bb47e1e60e383ea5526b4dd445
Max Bean <max.bean@omnimax.co.nz>
2025-11-20 16:15:07 +1300

    goop recipe first trial

--- data-stuff/science-goop.lua
+++ data-stuff/science-goop.lua
@@ -6,41 +6,70 @@ local base_texture = "__temp-mod__/graphics/fluids/bean.png" -- May need to set 
 local fluid_color = {0.9, 0.9, 0.9} -- !!! WIP (Trying white)
 local fluid_color_light = {1.0, 1.0, 1.0}
 
+-- !!! Mostly happy with lube, sulf, plast. Wood needs good looking at, and 4/5 need heavy amount balance
+-- !!! Tempted to output less goop per craft to inflate costs. Balance !!!
 local science_data = {
 	space = {
 		craft_category = "organic-or-chemistry",
 		ingredients = {
-			nil
+			{ type = "fluid", name = "lubricant", amount = 50 },
+			{ type = "fluid", name = "sulfuric-acid", amount = 50 },
+			{ type = "item", name = "plastic", amount = 5 },
+			{ type = "item", name = "uranium-fuel-cell", amount = 2 }, -- !!! Price check!
+			{ type = "item", name = "depleted-uranium-fuel-cell", amount = 1 } -- !!! Unsure if want both cells
 		}
 	},
 	metallurgic = {
 		craft_category = "metallurgy",
 		ingredients = {
-			nil
+			{ type = "fluid", name = "lubricant", amount = 50 },
+			{ type = "fluid", name = "sulfuric-acid", amount = 200 }, -- Extra acid
+			{ type = "item", name = "plastic", amount = 5 },
+			{ type = "item", name = "tungsten-carbide", amount = 3 }, -- !!! Price check!
+			{ type = "item", name = "refined-concrete", amount = 10 } -- !!! Price check!
+
 		}
 	},
 	agricultural = {
 		craft_category = "organic",
 		ingredients = {
-			nil
+			{ type = "fluid", name = "lubricant", amount = 50 },
+			{ type = "fluid", name = "sulfuric-acid", amount = 50 },
+			{ type = "item", name = "plastic", amount = 20 }, -- Extra (bio)plastic
+			{ type = "item", name = "nutrients", amount = 20 }, -- !!! Price check! (Likely a decent amount here tho, bioflux is cheap) (1biof is 60 after prod)
+			{ type = "item", name = "coal", amount = 10 } -- !!! Price check! Maybe coal, maybe explosives (!!! Test both? Prolly coal tho)
+
 		}
 	},
 	electromagnetic = {
 		craft_category = "electromagnetics",
 		ingredients = {
-			nil
+			{ type = "fluid", name = "lubricant", amount = 200 }, -- Extra lube
+			{ type = "fluid", name = "sulfuric-acid", amount = 50 },
+			{ type = "item", name = "plastic", amount = 5 },
+			{ type = "item", name = "superconductor", amount = 5 }, -- !!! PRICE
+			{ type = "item", name = "rocket-fuel", amount = 1 }, -- !!! PRICE (Also this one is uncertain)
 		}
 	},
 	cryogenic = {
 		craft_category = "cryogenics",
 		ingredients = {
-			nil
-		}
+			{ type = "fluid", name = "lubricant", amount = 50 },
+			{ type = "fluid", name = "sulfuric-acid", amount = 50 },
+			{ type = "item", name = "wood", amount = 5 }, -- Requires wood (!!! MAKE SPOIL, but also grow way faster. Heavy balance pass)
+			{ type = "item", name = "solid-fuel", amount = 5 }, -- !!! PRICE
+			utils.items.fluo_in(4) -- !!! PRICE AAA
+		},
+		byproduct = utils.items.fluo_out(2)
 	},
 	promethium = {
 		craft_category = "cryogenics",
 		ingredients = {
-			nil
+			{ type = "fluid", name = "lubricant", amount = 50 },
+			{ type = "fluid", name = "sulfuric-acid", amount = 50 },
+			{ type = "item", name = "wood", amount = 5 },
+			{ type = "item", name = "pentapod-egg", amount = 5 }, -- !!! PRICE (Overall 1/4 of b-egg I think I said? Check. Account for higher cost, risk and weight)
+			{ type = "item", name = "foundation", amount = 1 }, -- !!! PRICE (may be v expensive)
 		}
 	}
 }
@@ -81,7 +110,7 @@ for name, props in pairs(science_data) do
 		enabled = false,
 		energy_required = base_craft_time * util_props.craft_time_mult,
 		ingredients =  props.ingredients,
-		results = {{ type = "fluid", name = "science-goop", amount = 400 }}, -- 4 data crafts, so 2 pack crafts
+		results = {{ type = "fluid", name = "science-goop", amount = 400 }}, -- 4 data crafts, so 2 pack crafts (!!! I may change this ratio significantly !!! (See craft time too if so))
 		allow_productivity = true,
 		surface_conditions = util_props.surface_condition,
 		crafting_machine_tint = utils.recipe_tints(fluid_color)
