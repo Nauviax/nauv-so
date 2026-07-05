81f6c113f4d8b981486daa5cb7baaab5b2287af3
Nauviax <nauviax@icloud.com>
2026-07-05 17:35:19 +1200

    Made rockets more complicated

--- data-stuff/misc.lua
+++ data-stuff/misc.lua
@@ -19,3 +19,17 @@ pentapod["big-strafer-pentapod"].absorptions_to_join_attack.spores = 40
 
 -- Reduce speed of offshore pumps 1200/s -> 240/s, simply to require more of them.
 data.raw["offshore-pump"]["offshore-pump"].pumping_speed = 4 -- Old was 20
+
+-- Add e-engines to rocket parts, plus minor adjustments to the recipe.
+if settings.startup["nso-harder-rockets"] then
+	local rocket_part_recipe = data.raw.recipe["rocket-part"]
+	rocket_part_recipe.ingredients = {
+		{ type = "item", name = "processing-unit", amount = 2 },
+		{ type = "item", name = "low-density-structure", amount = 2 },
+		{ type = "item", name = "rocket-fuel", amount = 2 },
+		{ type = "item", name = "electric-engine-unit", amount = 1 }
+	}
+	rocket_part_recipe.energy_required = 10 -- Old was 3 (Or 6 for 2)
+	data.raw["rocket-silo"]["rocket-silo"].rocket_parts_required = 25 -- Old was 50. LDS etc per rocket is same.
+	data.raw.item["rocket-part"].weight = 40000 -- Old was 20000. Just to match per-rocket count to parts required.
+end
---
+++ settings.lua
@@ -0,0 +1,8 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "nso-harder-rockets",
+        setting_type = "startup",
+        default_value = true,
+    }
+})
